214576d5feafffe1117ce0f2e20ad82b5facf0f9
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-03-10 13:25:56 +0300

    addProductTocart()

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,6 +1,6 @@
 /* Create an array named products which you will use to add all of your product 
 object literals that you create in the next step. */
-let products = [productCherry,productStrawberry,productOrange];
+
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -35,10 +35,15 @@ let productOrange={
   productId:102,
   image:"images/orange.jpg"
 };
+let products = [productCherry,productStrawberry,productOrange];
+
 
 /* Declare an empty array named cart to hold the items in the cart */
 let cart = [];
 
+function getProductId(productId, productList){
+  return productList.find((product)=> product.productId === productId);
+}
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
@@ -46,39 +51,57 @@ let cart = [];
 */
 
 function addProductToCart(productId){
-  let product = products.productId;
-  
-
-
+  let product = getProductId (productId, products)
+  product.quantity ++;
+  if (!cart.includes(product)){
+    cart.push(product);
+  }
 }
+
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId){
 
+}
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId){
+
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart(productId){
+
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
+function cartTotal(){
+
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
-
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+function emptyCart(){
+  while (cart.length>0){
+    cart.pop();
+  }
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
