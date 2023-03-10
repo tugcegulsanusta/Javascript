7287c5f920aec293386ef277153d3c250d79baaa
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-03-10 14:47:56 +0300

    ready for first submission

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -14,114 +14,146 @@ object literals that you create in the next step. */
    - orange.jpg by Mae Mu
    - strawberry.jpg by Allec Gomes
 */
-let productCherry={
+let productCherry = {
   name: "Carton of Cherries",
   price: 4,
-  quantity:0,
-  productId:100,
-  image:"images/cherry.jpg"
+  quantity: 0,
+  productId: 100,
+  image: "images/cherry.jpg"
 };
-let productStrawberry={
+let productStrawberry = {
   name: "Carton of Strawberries",
   price: 5,
-  quantity:0,
-  productId:101,
+  quantity: 0,
+  productId: 101,
   image: "images/strawberry.jpg"
 };
-let productOrange={
-  name:"Bag of Oranges",
-  price:10,
-  quantity:0,
-  productId:102,
-  image:"images/orange.jpg"
+let productOrange = {
+  name: "Bag of Oranges",
+  price: 10,
+  quantity: 0,
+  productId: 102,
+  image: "images/orange.jpg"
 };
-let products = [productCherry,productStrawberry,productOrange];
+let products = [productCherry, productStrawberry, productOrange];
 
 
 /* Declare an empty array named cart to hold the items in the cart */
 let cart = [];
 
-function getProductId(productId, productList){
-  return productList.find((product)=> product.productId === productId);
-}
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
 
-function addProductToCart(productId){
-  let product = getProductId (productId, products)
-  product.quantity ++;
-  if (!cart.includes(product)){
-    cart.push(product);
+//additional function to 
+function getProductId(productId, productList) {
+  return productList.find((product) => product.productId === productId);
+}
+
+
+/* Create a function named addProductToCart that takes in the product productId as an argument
+- addProductToCart should get the correct product based on the productId
+- addProductToCart should then increase the product's quantity
+- if the product is not already in the cart, add it to the cart
+*/
+function addProductToCart(productId) {
+  const product = getProductId(productId, products);
+  product.quantity++;
+  if (!cart.includes(product)) {
+      cart.push(product);
   }
 }
 
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
+- increaseQuantity should get the correct product based on the productId
+- increaseQuantity should then increase the product's quantity
 */
-function increaseQuantity(productId){
-
+function increaseQuantity(productId) {
+  const product = getProductId(productId, products);
+  if (cart.includes(product)) {
+      product.quantity++;
+  } else {
+      addProductToCart(productId)
+  }
 }
+
+
 /* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
+- decreaseQuantity should get the correct product based on the productId
+- decreaseQuantity should decrease the quantity of the product
+- if the function decreases the quantity to 0, the product is removed from the cart
 */
-function decreaseQuantity(productId){
-
+function decreaseQuantity(productId) {
+  const product = getProductId(productId, products);
+  if (cart.includes(product)) {
+      product.quantity--;
+      if (product.quantity === 0) {
+          removeProductFromCart(productId)
+      }
+  }
 }
+
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
+- removeProductFromCart should get the correct product based on the productId
+- removeProductFromCart should update the product quantity to 0
+- removeProductFromCart should remove the product from the cart
 */
-function removeProductFromCart(productId){
-
+function removeProductFromCart(productId) {
+  const product = getProductId(productId, products);
+  product.quantity = 0;
+  cart.splice(cart.indexOf(product), 1);
 }
+
 
 /* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total of all products
-  - cartTotal should return the sum of the products in the cart
+- cartTotal should iterate through the cart to get the total of all products
+- cartTotal should return the sum of the products in the cart
 */
-function cartTotal(){
-
+function cartTotal() {
+  let total = 0;
+  for (let product of cart) {
+      total += product.quantity * product.price;
+  }
+  return total;
 }
 
+
 /* Create a function called emptyCart that empties the products from the cart */
-/* Create a function named pay that takes in an amount as an argument
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-*/
-function emptyCart(){
-  while (cart.length>0){
-    cart.pop();
+function emptyCart() {
+  while (cart.length > 0) {
+      cart.pop();
   }
+}
+
+
+/* Create a function named pay that takes in an amount as an argument
+- pay will return a negative number if there is a remaining balance
+- pay will return a positive number if money should be returned to customer
+*/
+function pay(amount) {
+  let total = 0;
+  total += amount;
+  return total - cartTotal();
 }
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 
 /* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
+ To fully complete this project, it is expected that all tests pass.
+ Run the following command in terminal to run tests
+ npm run test
 */
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  // currency
 }
